1b3b3ebbfebf32de8c7e078b0f0896d06ccc3106
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2018-09-16 21:32:40 -0400

    Remove format routing constraint (not needed)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      constraints format: 'json' do
-        resources :coach_directory, only: :index
-      end
+      resources :coach_directory, only: :index
     end
   end
 end
